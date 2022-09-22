9e9ead02bf5805f4705b95adf67df6bf8f81bb33
boualem06 <kb_hamroune@esi.dz>
2022-09-22 22:28:44 +0100

    adding the navbar and messages

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,14 +1,26 @@
 
 
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css';
+import Chat from './Components/Chat';
+import Navbarr from './Components/Navbarr'
+import Messages from './Components/Messages'
 function App() {
-
+  
   return (
-   <div>Home Page </div>
-
-  )
+    // <Router>
+    //   <Routes>
+    //     <Route element={<PrivateRoutes />}>
+    //       <Route path={'/'} element={<BlogList />} />
+    //       <Route path={'/NewBlog'} element={<NewBlog initialblog={initial} />} />
+    //       <Route path={'/MyBlogs'} element={< MyBlogs/>} />
+    //       MyBlogs
+    //     </Route>
+    //     <Route path={'/Signup'} element={<SignUp />} />
+    //     <Route exact path={'/login'} element={<Login />} />
+    //   </Routes>
+    // </Router>
+    <Chat></Chat>
+    
+  );
 }
 
 
--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily:{
-        lobster:['Lobster', 'cursive']
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
