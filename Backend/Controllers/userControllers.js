b0f797e0729c36799074f1ab5c916dd719507452
AhmedRamiBoukef <ahmedramiboukef@gmail.com>
2022-09-30 23:32:25 +0100

    add messages

--- Backend/Controllers/userControllers.js
+++ Backend/Controllers/userControllers.js
@@ -1,48 +1,47 @@
-const User=require('../models/userModel')
-const jwt=require('jsonwebtoken') ;
-const bcrypt=require('bcryptjs') ;
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 require("dotenv").config();
 // const asyncHandler=require('express-async-handler') ;
-const registerUser=async(req,res)=>{
-    const {name,email,password}=req.body ;
-    if(!name || !email || !password){
-        res.status(400) ;
-        res.json( {message:"please add all fields",status:400})
-        return ;
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400);
+        res.json({ message: "please add all fields", status: 400 })
+        return;
     }
 
     //check if user existe 
-    const userExist=await User.findOne({email}) ;
-    if(userExist)
-    {
-        res.status(400) ;
-        res.json({message:'user already exist',status:400})
-        return ;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        res.status(400);
+        res.json({ message: 'user already exist', status: 400 })
+        return;
     }
 
     //Hash password 
-    const salt=await bcrypt.genSalt(10) ;
-    const hashedPassword=await bcrypt.hash(password,salt) ;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     //create User
-    const user=await User.create({
+    const user = await User.create({
         name,
         email,
-        password:hashedPassword
+        password: hashedPassword
     })
 
-    if(user){
+    if (user) {
         res.status(201).json({
-            _id:user.id,
-            name:user.name,
-            email:user.email,
-            status:201,
-            token:generateToken(user.id)
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            status: 201,
+            token: generateToken(user.id)
         })
     }
-    else{
-        res.status(400) ;
-        res.json({message:'invalid user data ',status:400})
+    else {
+        res.status(400);
+        res.json({ message: 'invalid user data ', status: 400 })
         // throw new Error('invalid user data ')
     }
 }
@@ -51,52 +50,64 @@ const registerUser=async(req,res)=>{
 
 
 
-const loginUser=async (req,res)=>{
-    const {email,password}=req.body ;
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
     // check for user email 
-    const user=await User.findOne({email}) ;
-    if(user && (await bcrypt.compare(password,user.password)))
-    {
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
         res.json({
-            _id:user.id,
-            name:user.name,
-            email:user.email,
-            status:201,
-            token:generateToken(user.id)
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            status: 201,
+            token: generateToken(user.id)
         })
-    }else
-    {
-        res.status(400) ;
-        res.json({message:'Invalide credentials',status:400})
+    } else {
+        res.status(400);
+        res.json({ message: 'Invalide credentials', status: 400 })
         // throw new Error('Invalide credentials')
     }
 }
 
 // Generate Token
 
-const generateToken=(id)=>{
-    return jwt.sign({id},process.env.JWT_SECRET,{
-        expiresIn:'30d',
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
     })
 }
 
 
 
 //get the actuell user
-const me=async (req,res)=>{
+const me = async (req, res) => {
     const {_id,name,email}=await User.findById(req.user.id) ;
     res.status(200).json({
         id:_id,
         name,
         email
     })
-} 
+  
+}
 
 
-module.exports={
+const getUser = async (req, res) => {
+
+    const user = await User.findById(req.params.userId);
+    res.status(200).json(user);
+}
+
+const allUsers=async (req,res)=>{
+    const users= await User.find() ;
+    res.status(200).json(users);
+}
+
+module.exports = {
     registerUser,
     loginUser,
-    me
-    
+    me,
+    getUser,
+    allUsers
+
 }
--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,27 @@
 const express = require('express')
 const { default: mongoose } = require('mongoose')
-
-const UserRoutes = require("./routes/UserRoutes")
+const UserRoutes = require("./routes/UserRoutes") ;
+const conversationRoutes=require('./routes/conversation')
+const messageRoutes=require('./routes/messages')
 const app = express()
 const cors = require("cors");
 require("dotenv").config();
 
 const port = 5000
-// const dbURI = "le lien de la base de donne"
+// const dbURI = "mongodb+srv://Boualem:boualemhamroune1649@blogs.xwwjje4.mongodb.net/?retryWrites=true&w=majority"
+const dbURI="mongodb://Boualem:boualemhamroune1649@ac-5kr3may-shard-00-00.palryrg.mongodb.net:27017,ac-5kr3may-shard-00-01.palryrg.mongodb.net:27017,ac-5kr3may-shard-00-02.palryrg.mongodb.net:27017/?ssl=true&replicaSet=atlas-cs2cfm-shard-0&authSource=admin&retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => { console.log("connected to db"); app.listen(port); })
+  .catch((err) => { console.log(err) });
 
-
-//mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//  .then((result) => { console.log("connected to db"); app.listen(port); })
-  //.catch((err) => { console.log(err) });
-
-//app.use(cors())
-//app.use(express.json({ limit: "1mb" }));
-//app.use(express.static('public'));
-//app.use(UserRoutes);
+app.use(cors())
+app.use(express.json({ limit: "1mb" }));
+app.use(express.static('public'));
+app.use("/user",UserRoutes);
+app.use("/conversation",conversationRoutes) ;
+app.use("/messages",messageRoutes) ;
 
  
   
-// )
 
 
